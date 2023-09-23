4d7bff6e420ccf9603434831f9e9f7dabf39f5dd
Daniel Longacre <danieljlongacre@gmail.com>
2023-09-23 16:00:46 -0400

    Added all functions that query for viewing the DB

--- server.js
+++ server.js
@@ -1,30 +1,51 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+// require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //Connect to the database
-const db = `mysql://root:root@localhost:3306/employeetracker`;
+// const db = `mysql://root:root@localhost:3306/employeetracker`;
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'employeetracker',
+    password: 'root'
+});
+
+connection.connect(
+    (error) => {
+        if(error) {
+            console.log(error)
+        } else {
+            menu();
+        }
+    }
+)
 
 //Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-const questions = () => {
+const menu = () => {
     inquirer.prompt([
         {
             type: 'list',
             name: 'choice',
             message: 'What would you like to do?',
-            choices: ['Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
         }
     ])
     .then((answers) => {
         console.log(answers)
-        if(answers.choice === 'Add Employee') {
+        if(answers.choice === 'View All Employees') {
+            console.log()
+            viewAllEmployees()
+        }
+        else if(answers.choice === 'Add Employee') {
             addEmployee()
         }
         else if(answers.choice === 'Update Employee Role') {
@@ -49,9 +70,20 @@ const questions = () => {
     });
 }
 
+const viewAllEmployees = () => {
+    connection.query(
+        `SELECT e.id, e.first_name, e.last_name, r.title, d.name as department, r.salary FROM employee e LEFT JOIN role r on r.id = e.role_id LEFT JOIN department d on d.id = r.department_id`,
+        function(err, results) {
+            console.log(err);
+            console.table(results);
+            menu()
+        }
+    );
+}
+
 // Add employee function
 const addEmployee = () => {
-    db.query('select * from role', function(err, res) {
+    connection.query('select * from role', function(err, res) {
         const roleList = res.map(({ id, title }) => ({
             name: title,
             value: id
@@ -85,9 +117,32 @@ const addEmployee = () => {
     })
 }
 
+const viewAllRoles = () => {
+    connection.query(
+            `SELECT * FROM role`,
+            function(err, results) {
+                console.log(err);
+                console.table(results);
+                menu()
+            }
+    );
+}
+
+
+const viewAllDepartments = () => {
+    connection.query(
+        `SELECT * FROM department`,
+        function(err, results) {
+            console.log(err);
+            console.table(results);
+            menu()
+        }
+    );
+}
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 })
 
-questions();
+// menu();
