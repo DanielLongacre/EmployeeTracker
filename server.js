78646a0023a93f02ff5ed5f111c557d774a3f9f0
Daniel Longacre <danieljlongacre@gmail.com>
2023-09-23 16:41:33 -0400

    Finished addEmployee function and added updateEmployeeRole

--- server.js
+++ server.js
@@ -73,41 +73,89 @@ const viewAllEmployees = () => {
     );
 }
 
-// Add employee function
+
 const addEmployee = () => {
-    connection.query('select * from role', function(err, res) {
-        const roleList = res.map(({ id, title }) => ({
-            name: title,
-            value: id
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "first_name",
+            message: "What is the employee's first name?"
+        },
+        {
+            type: "input",
+            name: "last_name",
+            message: "What is the employee's last name?"
+        },
+        {
+            type: "list",
+            name: "role_id",
+            message: "What is the employee's role?",
+            choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer', 'Customer Service']
+        },
+        {
+            type: "list",
+            name: "manager_id",
+            message: "Who is the employee's manager",
+            choices: ['None', 'Drake', 'Taylor Swift', 'Kenny Chesney', 'Morgan Wallen', 'Jesus']
+        }
+    ])
+        .then((answers) => {
+            console.log(answers)
+            connection.query(
+                `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES('${answers.first_name}', '${answers.last_name}', '${answers.role_id}', '${answers.manager_id}')`,
+                function (err, results) {
+                    console.log(err);
+                    console.table(results); // results contains rows returned by server
+
+                }
+            )
+        })
+}
+
+
+const updateEmployeeRole = () => {
+
+    connection.query('select * from employee', function (err, res) {
+        const employeeList = res.map(({id, first_name, last_name}) => ({
+            name: first_name + ' ' + last_name,
+            value: id, 
         }))
 
-        inquirer.prompt([
-            {
-                type: input,
-                name: firstName,
-                message: "What is the employee's first name?"
-            },
-            {
-                type: input,
-                name: lastName,
-                message: "What is the employee's last name?"
-            },
-            {
-                type: input,
-                name: role,
-                message: "What is the employee's role?",
-                choices: roleList
-            }
-        ])
-        .then((answers) => {
-            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES('${answers.firstName}', '${answers.lastName}', ${anaswers.role});`, function(err, res) {
+     connection.query('select * from role', function (err, res) {
+            const roleList = res.map(({ id, title }) => ({
+                name: title ,
+                value: id
+            }))
+  
+     inquirer.prompt([
+        {
+            type: "list",
+            name: "updateER",
+            message: "Which employee's role do you want to update?",
+            choices: employeeList
+        },
+        {
+            type: "list",
+            name: "updateRoleTitle",
+            message: "Which role do you want to assign the selected employee?",
+            choices: roleList
+        }
+    ])
+    .then((answers) => {
+        console.log(answers)
+        connection.query(
+            `UPDATE employee SET role_id = ('${answers.updateRoleTitle}') WHERE employee.role_id`,
+            function (err, results) {
                 console.log(err);
-                console.log(`Added ${res.firstName} ${res.lastName} to the database`);
-                questions();
-            })
-        })
+                console.log('new role has been added!'); // results contains rows returned by server
+                askUser()
+            }
+        )
     })
+   })
+ })
 }
+
 
 const viewAllRoles = () => {
     connection.query(
